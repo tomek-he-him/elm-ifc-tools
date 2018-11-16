79723359381d2f500fdd6e11a1418c3fb4facf34
Tomek Wiszniewski <tomek@wiszniewski.cc>
2018-11-16 09:37:47 +0100

    Make IfcProject idiomatic

--- src/Ifc/Entity.elm
+++ src/Ifc/Entity.elm
@@ -1,10 +1,31 @@
 module Ifc.Entity exposing (ifcProject)
 
 import Ifc.Guid as Guid exposing (Guid)
-import Iso10303 as Step exposing (Attribute, Entity)
+import Ifc.Types exposing (label, optional)
+import Iso10303 as Step exposing (Attribute, Entity, list, null, referenceTo, string)
 
 
-ifcProject : Guid -> List Attribute -> Entity
-ifcProject guid attributes =
+ifcProject :
+    { guid : Guid
+    , ownerHistory : Maybe Entity
+    , name : Maybe String
+    , description : Maybe String
+    , objectType : Maybe String
+    , longName : Maybe String
+    , phase : Maybe String
+    , representationContexts : Maybe (List Entity)
+    , unitsInContext : Maybe Entity
+    }
+    -> Entity
+ifcProject { guid, ownerHistory, name, description, objectType, longName, phase, representationContexts, unitsInContext } =
     Step.entity "IfcProject"
-        (Step.string (Guid.toString guid) :: attributes)
+        [ string (Guid.toString guid)
+        , optional referenceTo ownerHistory
+        , optional label name
+        , optional string description
+        , optional label objectType
+        , optional label longName
+        , optional label phase
+        , optional (list referenceTo) representationContexts
+        , optional referenceTo unitsInContext
+        ]
