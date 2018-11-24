7c841cc43b3cb3a59bd8f450d4ba74cee035868c
Tomek Wiszniewski <tomek@wiszniewski.cc>
2018-11-24 22:24:52 +0100

    Mock up chain-based API

--- src/Ifc/Utils.elm
+++ src/Ifc/Utils.elm
@@ -1,53 +1,90 @@
-module Ifc.Utils exposing (withRelatedUniqueEntities, withUniqueEntity)
+module Ifc.Utils exposing (withDependentEntities, withRootEntity)
 
 import Ifc.Entity exposing (UniqueEntity)
 import Ifc.Guid exposing (Guid, randomGuid)
 import Iso10303 exposing (Entity)
+import List.Nonempty exposing (Nonempty)
 import Random exposing (Seed)
 
 
-{-| Add a `UniqueEntity` with a randomly generated GUID to a list of IFC entities.
+{-| Functions in this module operate on a pair of values: a non-empty list of
+entities and a random seed.
+-}
+type alias Entities =
+    ( Nonempty Entity, Seed )
+
+
+{-| Start building your list of `Entities` with a single unique entity. Most
+often it’ll be `ifcProject` that’s required in any IFC model.
 
 Example:
 
     ( entities, newSeed ) =
-        ( [], initialSeed 0 )
-            |> withUniqueEntity ifcProject { … }
-            |> withUniqueEntity ifcBuilding { … }
+        initialSeed 0
+            |> withRootEntity ifcProject { … }
             …
+            |> toEntitiesAndSeed
 
 -}
-withUniqueEntity : UniqueEntity attributes -> attributes -> ( List Entity, Seed ) -> ( List Entity, Seed )
-withUniqueEntity uniqueEntity attributes ( entities, currentSeed ) =
+withRootEntity : UniqueEntity attributes -> attributes -> Seed -> Entities
+withRootEntity uniqueEntity attributes seed =
     let
         ( guid, newSeed ) =
-            Random.step randomGuid currentSeed
+            Random.step randomGuid seed
     in
-    ( uniqueEntity attributes guid :: entities
+    ( List.Nonempty.fromElement (uniqueEntity attributes guid)
     , newSeed
     )
 
 
-{-| Add a pair of unique entities with randomly generated GUIDs that need to reference each other.
+{-| Add a bunch of unique entities that need to reference another unique entity.
+
+The factory function takes a rendered unique entity and returns a non-empty list
+of unique entities. This means that you have access to the rendered entity while
+building that list of entities. This makes it easy to build IFC relationships
+like `ifcRelAggregates` or `ifcRelContainedInSpatialStructure`.
+
+See the example on how to nest this function if you need access to more than one
+entity.
 
 Example:
 
-    ( entities, newSeed ) =
-        ( [], initialSeed 0 )
-            |> withUniqueEntity ifcProject …
-            |> withRelatedUniqueEntities ( ifcBuilding { … }, ifcWall { … } )
-                (\(buildingEntity, wallEntity) ->
-                    ifcRelContainedInSpatialStructure
-                        { …
-                        , relatedElements = [ wallEntity ]
-                        , relatingStructure = buildingEntity
-                        }
-                )
-            …
+    let
+        ( entities, newSeed ) =
+            ( [], initialSeed 0 )
+                |> withRootEntity ifcProject …
+                |> withDependentEntities buildingInProject
+                |> withDependentEntities wallsInBuilding
+                …
+                |> asListAndSeed
+
+        buildingInProject renderedProject =
+            List.Nonempty.Nonempty building
+                [ ifcRelAggregates
+                    { relatingObject = renderedProject
+                    , relatedObjects = [ building ]
+                    , …
+                    }
+                ]
+
+        wallsInBuilding renderedBuilding =
+            List.Nonempty.fromElement material
+                |> withDependentEntities
+                    (\renderedMaterial ->
+                        wall renderedBuilding renderedMaterial { … }
+                            |> List.Nonempty.append (wall renderedBuilding renderedMaterial { … })
+                            |> List.Nonempty.append (wall renderedBuilding renderedMaterial { … })
+                    )
+
+        building =
+            ifcBuilding { … }
+
+        wall renderedBuilding renderedMaterial =
+            List.Nonempty.Nonempty
 
 -}
-withRelatedUniqueEntities : ( Guid -> Entity, Guid -> Entity ) -> (( Entity, Entity ) -> Guid -> Entity) -> ( List Entity, Seed ) -> ( List Entity, Seed )
-withRelatedUniqueEntities ( entityWithGuid1, entityWithGuid2 ) entityWithRelations ( entities, currentSeed ) =
+withDependentEntities : ( Guid -> Entity, Guid -> Entity ) -> (( Entity, Entity ) -> Guid -> Entity) -> ( List Entity, Seed ) -> ( List Entity, Seed )
+withDependentEntities ( entityWithGuid1, entityWithGuid2 ) entityWithRelations ( entities, currentSeed ) =
     let
         ( guid1, newSeed1 ) =
             Random.step randomGuid currentSeed
